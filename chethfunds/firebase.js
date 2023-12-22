024aa6624da7de4fd5b1491cd7657fc3e6c20c94
Somnathumapathi <somnathumapathi7@gmail.com>
2023-12-22 21:49:07 +0530

    Realtime firebase logic

--- chethfunds/firebase.js
+++ chethfunds/firebase.js
@@ -1,26 +1,13 @@
 
-// import firebase from 'firebase/app';
-// import 'firebase/auth';
-// import 'firebase/database';
 
-// const firebaseConfig = {
-//   apiKey: 'YOUR_API_KEY',
-//   authDomain: 'YOUR_AUTH_DOMAIN',
-//   projectId: 'YOUR_PROJECT_ID',
-//   storageBucket: 'YOUR_STORAGE_BUCKET',
-//   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-//   appId: 'YOUR_APP_ID',
-// };
-
-// if (!firebase.apps.length) {
-//   firebase.initializeApp(firebaseConfig);
-// }
 'use client';
 
 // export default firebase;
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,10 +18,13 @@ const firebaseConfig = {
   projectId: "chethfunds",
   storageBucket: "chethfunds.appspot.com",
   messagingSenderId: "851022073074",
-  appId: "1:851022073074:web:ed76d6e65e69c1f4bf546b"
+  appId: "1:851022073074:web:ed76d6e65e69c1f4bf546b",
+
 };
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
 const auth = getAuth(firebase)
-export default auth;
+const firestore = getFirestore(firebase);
+
+export { auth, firestore };
