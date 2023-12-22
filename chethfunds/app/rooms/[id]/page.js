b5c6e81130ca8a2b5018bc2658259cf0fee6c73c
Koushik <koushik.mbp@gmail.com>
2023-12-22 14:35:36 +0530

    Changes

--- chethfunds/app/rooms/[id]/page.js
+++ chethfunds/app/rooms/[id]/page.js
@@ -1,12 +1,31 @@
 'use client';
-const DynamicPage = ({ params: { id } }) => {
+let date=22;
+const currentDate=new Date();
+const day = currentDate.getDate();
+// const depobid = () => {
+//     if(date===day){
+//         <button>Deposit</button>
+        
+//     }
+// }
 
+const DynamicPage = ({ params: { id } }) => {
     return (
         <div>
-            <h1>Chitfund Room Page</h1>
+            <h1 className="font-bold text-2xl">Chitfund Room Page</h1>
             <p>ID: {id}</p>
             <br />
-            <p>Starting Date:</p>
+            <p>Starting Date of this room: {date} </p>
+            {/* <p>{`Deposit/Bid on ${date} of every month`}</p> */}
+            {
+                date===day ? 
+                <>
+                <button>Deposit</button> <br/> <br/>
+                <button>Bid</button>
+                </>
+                : 
+                <button>{`Come back on ${date} for depositing or bidding`}</button>
+            }
         </div>
     );
 };
