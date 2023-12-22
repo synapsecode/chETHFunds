4d4e6ea66debf3bbb06413a7749b849606f24c1b
Manas Hejmadi <manas2all@outlook.com>
2023-12-22 09:58:07 +0530

    created contracts: blank Hardhat project

---
+++ contracts/hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: `${process.env.ALCHEMY_GOERLI_URL}`,
+      accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
+    }
+  }
+};
