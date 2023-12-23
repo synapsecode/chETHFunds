314fdce201d94dd25cbc8114a33f001bc810245c
Manas Hejmadi <manas2all@outlook.com>
2023-12-23 08:27:02 +0530

    RoomsPage: Removed RoomList for demo purpose

--- chethfunds/app/rooms/page.js
+++ chethfunds/app/rooms/page.js
@@ -105,7 +105,7 @@ export default function RoomHome() {
                     </div>
 
 
-                    <RoomComponent />
+                    {/* <RoomComponent /> */}
 
                 </div>
             </center>
